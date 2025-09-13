7a497d0f259aa8e9e1438147105348052245522b
RitikPadhy <ritipkp2002@gmail.com>
2025-09-14 00:35:15 +0530

    fix for deployment

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -2,7 +2,7 @@ import os
 from firebase_admin import credentials, firestore, initialize_app
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-cred_path = os.path.join(BASE_DIR, "config/firebase-service-account.json")
+cred_path = os.path.join(BASE_DIR, "firebase-service-account.json")
 
 cred = credentials.Certificate(cred_path)
 initialize_app(cred)
