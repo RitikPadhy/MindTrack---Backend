5678ca4f13f213f3f3e952f8207842acd90ad8d2
RitikPadhy <ritipkp2002@gmail.com>
2025-09-14 00:44:24 +0530

    fix for deployment

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -1,10 +1,10 @@
-import os
-from firebase_admin import credentials, firestore, initialize_app
+import firebase_admin
+from firebase_admin import credentials, firestore
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-cred_path = os.path.join(BASE_DIR, "firebase-service-account.json")
+# Path to secret file provided by Render
+cred_path = "/etc/secrets/firebase-service-account.json"
 
 cred = credentials.Certificate(cred_path)
-initialize_app(cred)
+firebase_admin.initialize_app(cred)
 
 db = firestore.client()
